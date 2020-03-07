4448838fd8c41a409e5ddbba702ec7b7545f445a
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-07 13:06:38 -0500

    saving this lab for later

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,24 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility <E> {
+
+    private E[] masterArray;
+    private ArrayList<E> masterList;
+
+    public ArrayUtility(E[] inputArray) {
+        this.masterList = new ArrayList<E>(Arrays.asList(inputArray));
+        this.masterArray = inputArray;
+    }
+
+    public E[] removeValue(Integer value) {
+        masterList.remove(value);
+
+    }
+
 }
