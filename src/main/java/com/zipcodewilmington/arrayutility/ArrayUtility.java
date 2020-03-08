d1adeab3f227ec9f432074ac7e89664778270fd0
Giles Bradford <gbradford@Gbrads-Mac.fios-router.home>
2020-03-07 23:50:11 -0500

    Completed lab

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,8 @@
 package com.zipcodewilmington.arrayutility;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by leon on 3/6/18.
@@ -9,16 +10,87 @@ import java.util.Arrays;
 public class ArrayUtility <E> {
 
     private E[] masterArray;
-    private ArrayList<E> masterList;
 
     public ArrayUtility(E[] inputArray) {
-        this.masterList = new ArrayList<E>(Arrays.asList(inputArray));
         this.masterArray = inputArray;
     }
 
-    public E[] removeValue(Integer value) {
-        masterList.remove(value);
+    public E[] removeValue(E value) {
+        @SuppressWarnings("unchecked")
+        E[] buffer = (E[]) Array.newInstance(value.getClass(), masterArray.length);
+        int index = 0;
 
+        for (int i = 0; i < masterArray.length; i++) {
+            if (masterArray[i] != value) {
+                buffer[index++] = masterArray[i];
+            }
+        }
+        @SuppressWarnings("unchecked")
+        E[] output = (E[]) Array.newInstance(value.getClass(), index);
+
+        for (int i = 0; i < index; i++) {
+            output[i] = buffer[i];
+        }
+
+        return output;
+    }
+
+    public Integer countDuplicatesInMerge(E[] arrayToMerge, E value) {
+        int count = 0;
+
+        for (E e : arrayToMerge) {
+            if (e == value) {
+                count++;
+            }
+        }
+
+        for (E e : masterArray) {
+            if (e == value) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public E getMostCommonFromMerge(E[] arrayToMerge) {
+        HashMap<E, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < masterArray.length; i++) {
+            if (map.containsKey(masterArray[i])) {
+                map.put(masterArray[i], map.get(masterArray[i]) + 1);
+            } else
+                map.put(masterArray[i], 1);
+        }
+
+        for (int i = 0; i < arrayToMerge.length; i++) {
+            if (map.containsKey(arrayToMerge[i])) {
+                map.put(arrayToMerge[i], map.get(arrayToMerge[i]) + 1);
+            } else
+                map.put(arrayToMerge[i], 1);
+        }
+
+        int value = 0;
+        E key = null;
+
+        for (Map.Entry<E, Integer> element : map.entrySet()) {
+            if (element.getValue() > value) {
+                value = element.getValue();
+                key = element.getKey();
+            }
+        }
+        return key;
+    }
+
+    public Integer getNumberOfOccurrences(E valueToEvaluate) {
+        int count = 0;
+
+        for (int i = 0; i < masterArray.length; i++) {
+            if (masterArray[i] == valueToEvaluate) {
+                count++;
+            }
+        }
+        return count;
     }
 
 }
